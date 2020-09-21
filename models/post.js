9a46b5950534bd8b0bf3f14b1de86ef897b3b754
Phil DeLong <philjdelong@gmail.com>
2020-09-21 12:30:05 -0600

    Remove commented alternative

--- models/post.js
+++ models/post.js
@@ -17,24 +17,3 @@ const postSchema = new Schema({
 });
 
 module.exports = mongoose.model('posts', postSchema);
-// const getDB = require('../utils/dbConnection').getDB
-// const mongoDB = require('mongodb');
-// const { post } = require('../routes/feed');
-
-
-// class Post {
-//     constructor(title, content, imageURL) {
-//         this.title = title,
-//         this.content = content,
-//         this.imageURL = imageURL
-//     }
-
-//     save() {
-//         const getDB = getDB();
-//         db.collection('posts').insertOne(this).then().catch(err => {
-//             console.log(err);
-//         })
-//     }
-// }
-
-// module.exports = mongoDB('post', post)
