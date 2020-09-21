0ff162e4970aa1a229b6c607ec4a8cd917b79f48
Phil DeLong <philjdelong@gmail.com>
2020-09-21 13:15:24 -0600

    Update console to log string

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const mongoConnect = () => {
     mongoose.connect('mongodb+srv://phil:delong@feedapi.1q4s7.gcp.mongodb.net/apipractice2?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
         .then(result => {
-            console.log(result.models);
+            console.log("DB Connected");
         })
         .catch(err => {
             console.log(err);
