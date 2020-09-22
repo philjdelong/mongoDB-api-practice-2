fde52b0fecf978148ff59a8dc0201bcea159a12a
Phil DeLong <philjdelong@gmail.com>
2020-09-22 14:36:23 -0600

    Add user routes

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const feedRoutes = require('./routes/feed');
 const connection = require('./utils/dbConnection').mongoConnect;
+
+const feedRoutes = require('./routes/feed');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/", feedRoutes)
+app.use("/api/v1", feedRoutes)
+app.use("/user", userRoutes)
 
 connection(
     app.listen(3000),
