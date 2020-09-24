a3918a6658bcf5870771f00c87ec2ad58fc28d97
Phil DeLong <philjdelong@gmail.com>
2020-09-24 09:48:57 -0600

    Create auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,19 @@
+// const session = require('express-session');
+const User = require('../models/user');
+
+exports.userLogin = (req, res, next) => {
+    const email = req.headers.email
+    const password = req.headers.password
+    User.find({email})
+        .then(user => {
+            if(password == user[0].password) {
+                res
+                    .status(200)
+                    .redirect('/user/dashboard');
+            } else {
+                res.status(401).json({
+                    data: "Login not successful, Please try again."
+                });
+            }
+        })
+}
