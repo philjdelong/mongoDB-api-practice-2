bc86bdd26fd3188b9d18b6509e0d6c4ffe5e497c
Phil DeLong <philjdelong@gmail.com>
2020-09-24 09:48:38 -0600

    Include auth route

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const connection = require('./utils/dbConnection').mongoConnect;
 
 const feedRoutes = require('./routes/feed');
 const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/api/v1", feedRoutes)
+app.use("/", feedRoutes)
 app.use("/user", userRoutes)
+app.use("/auth", authRoutes)
 
 connection(
     app.listen(3000),
